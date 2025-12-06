4a93b947b57504c53d7c70dca833af5bac8b888a
Shivam Kohli <shivamkohli810@gmail.com>
2025-12-06 16:47:59 +0100

    Time: 100 ms (45.50%) | Memory: 26.9 MB (32.63%) - LeetSync

---
+++ Two/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,55 @@
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #brute force--time limit Exceeded
+        #O(n^2)- Time Complexity
+        
+        '''max_profit=0
+        for i in range(len(prices)):
+            for j in range((i+1),len(prices)):
+                if prices[i]<prices[j]:
+                    profit=prices[j]-prices[i]
+                    max_profit=max(max_profit,profit)
+        return max_profit'''
+        
+        #O(n)-Time Complexity
+        max_profit=0
+        left=0
+        right=left+1
+        while (right<len(prices)):
+            if prices[left]<prices[right]:
+                profit=prices[right]-prices[left]
+                max_profit=max(max_profit,profit)
+            else:
+                left =right
+            right=right+1
+        return max_profit
+             
+
+        '''j=len(prices)-1
+        while(i<j):
+            if prices[i]<prices[j]:
+                profit=prices[j]-prices[i]
+                max_profit=max(max_profit,profit)
+                if prices[i+1]<prices[i] and i+1<j:
+                    i=i+1
+                else:
+                    j=j-1
+                
+            else:
+                if prices[j-1]>prices[j] and i<j-1:
+                    j=j-1
+                else:
+                    i=i+1
+
+        return max_profit'''
+            
+            
+
+
+
+
+
+
+        
+        
